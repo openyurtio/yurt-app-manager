d3ec56e97ce9e79a49ea86f0efeecc85dc65edb5
manyi <manyi@B-5241MD6P-0125.local>
2022-08-24 16:08:32 +0800

    update yurtappdaemon_controller_test.go

--- pkg/yurtappmanager/controller/yurtappdaemon/yurtappdaemon_controller_test.go
+++ pkg/yurtappmanager/controller/yurtappdaemon/yurtappdaemon_controller_test.go
@@ -17,112 +17,169 @@ limitations under the License.
 package yurtappdaemon
 
 import (
+	"context"
 	"reflect"
 	"testing"
 
 	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	unitv1alpha1 "github.com/openyurtio/yurt-app-manager/pkg/yurtappmanager/apis/apps/v1alpha1"
 	"github.com/openyurtio/yurt-app-manager/pkg/yurtappmanager/controller/yurtappdaemon/workloadcontroller"
 )
 
-//var cfg *rest.Config
-//var testenv *envtest.Environment = &envtest.Environment{}
-//var option manager.Options
-//var rl resourcelock.Interface
-//
-//// clientTransport is used to force-close keep-alives in tests that check for leaks.
-//var clientTransport *http.Transport
-//
-//func init() {
-//	testenv = &envtest.Environment{}
-//	cfg, _ = testenv.Start()
-//	option = manager.Options{
-//		NewCache: func(config *rest.Config, opts cache.Options) (cache.Cache, error) {
-//			return nil, fmt.Errorf("expected error")
-//		},
-//	}
-//}
+func TestAdd(t *testing.T) {
+	cfg, _ := config.GetConfig()
+	mgr, _ := manager.New(cfg, manager.Options{})
+	tests := []struct {
+		name   string
+		mgr    manager.Manager
+		cxt    context.Context
+		expect error
+	}{
+		{
+			name:   "add new key/val",
+			mgr:    mgr,
+			cxt:    context.TODO(),
+			expect: nil,
+		},
+	}
+	for _, tt := range tests {
+		st := tt
+		tf := func(t *testing.T) {
+			t.Parallel()
+			t.Logf("\tTestCase: %s", st.name)
+			{
+				get := Add(tt.mgr, tt.cxt)
+				if !reflect.DeepEqual(get, st.expect) {
+					t.Fatalf("\t%s\texpect %v, but get %v", failed, st.expect, get)
+				}
+				t.Logf("\t%s\texpect %v, get %v", succeed, st.expect, get)
 
-//func TestAdd(t *testing.T) {
-//	tests := []struct {
-//		name   string
-//		mng    manager.Manager
-//		cxt    context.Context
-//		expect map[string]string
-//	}{
-//		{
-//			"add new key/val",
-//			manager.New(cfg, option),
-//			context.TODO(),
-//			map[string]string{
-//				"foo": "bar",
-//				"buz": "qux",
-//			},
-//		},
-//	}
-//	for _, tt := range tests {
-//		st := tt
-//		tf := func(t *testing.T) {
-//			t.Parallel()
-//			t.Logf("\tTestCase: %s", st.name)
-//			{
-//				get := Add()
-//				if !reflect.DeepEqual(get, st.expect) {
-//					t.Fatalf("\t%s\texpect %v, but get %v", failed, st.expect, get)
-//				}
-//				t.Logf("\t%s\texpect %v, get %v", succeed, st.expect, get)
-//
-//			}
-//		}
-//		t.Run(st.name, tf)
-//	}
-//}
+			}
+		}
+		t.Run(st.name, tf)
+	}
+}
 
-//func TestUpdateStatus(t *testing.T) {
-//	tests := []struct {
-//		name            string
-//		instance        *unitv1alpha1.YurtAppDaemon
-//		newStatus       *unitv1alpha1.YurtAppDaemonStatus
-//		oldStatus       *unitv1alpha1.YurtAppDaemonStatus
-//		currentRevision *appsv1.ControllerRevision
-//		collisionCount  int32
-//		templateType    unitv1alpha1.TemplateType
-//		expect          reconcile.Result
-//	}{
-//		{
-//			"equal",
-//			&unitv1alpha1.YurtAppDaemon{},
-//			&unitv1alpha1.YurtAppDaemonStatus{
-//
-//			},
-//			&unitv1alpha1.YurtAppDaemonStatus{},
-//			&appsv1.ControllerRevision{},
-//			1,
-//			"StatefulSet",
-//			reconcile.Result{},
-//		},
-//	}
-//
-//	for _, tt := range tests {
-//		st := tt
-//		tf := func(t *testing.T) {
-//			t.Parallel()
-//			t.Logf("\tTestCase: %s", st.name)
-//			{
-//				rc := &ReconcileYurtAppDaemon{}
-//				get, _ := rc.updateStatus(
-//					st.instance, st.newStatus, st.oldStatus, st.currentRevision, st.collisionCount, st.templateType)
-//				if !reflect.DeepEqual(get, st.expect) {
-//					t.Fatalf("\t%s\texpect %v, but get %v", failed, st.expect, get)
-//				}
-//				t.Logf("\t%s\texpect %v, get %v", succeed, st.expect, get)
-//
-//			}
-//		}
-//		t.Run(st.name, tf)
-//	}
-//}
+func TestNewReconciler(t *testing.T) {
+	cfg, _ := config.GetConfig()
+	mgr, _ := manager.New(cfg, manager.Options{})
+	tests := []struct {
+		name   string
+		mgr    manager.Manager
+		expect reconcile.Reconciler
+	}{
+		{
+			name: "add new key/val",
+			mgr:  mgr,
+			expect: &ReconcileYurtAppDaemon{
+				Client: mgr.GetClient(),
+				scheme: mgr.GetScheme(),
+
+				recorder: mgr.GetEventRecorderFor(controllerName),
+				controls: map[unitv1alpha1.TemplateType]workloadcontroller.WorkloadControllor{
+					//			unitv1alpha1.StatefulSetTemplateType: &StatefulSetControllor{Client: mgr.GetClient(), scheme: mgr.GetScheme()},
+					unitv1alpha1.DeploymentTemplateType: &workloadcontroller.DeploymentControllor{Client: mgr.GetClient(), Scheme: mgr.GetScheme()},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		st := tt
+		tf := func(t *testing.T) {
+			t.Parallel()
+			t.Logf("\tTestCase: %s", st.name)
+			{
+				get := newReconciler(tt.mgr)
+				if !reflect.DeepEqual(get, st.expect) {
+					t.Fatalf("\t%s\texpect %v, but get %v", failed, st.expect, get)
+				}
+				t.Logf("\t%s\texpect %v, get %v", succeed, st.expect, get)
+
+			}
+		}
+		t.Run(st.name, tf)
+	}
+}
+
+func TestUpdateStatus(t *testing.T) {
+	var int1 int32 = 11
+	var yad *unitv1alpha1.YurtAppDaemon
+	yad = &unitv1alpha1.YurtAppDaemon{}
+	yad.Generation = 1
+
+	tests := []struct {
+		name            string
+		instance        *unitv1alpha1.YurtAppDaemon
+		newStatus       *unitv1alpha1.YurtAppDaemonStatus
+		oldStatus       *unitv1alpha1.YurtAppDaemonStatus
+		currentRevision *appsv1.ControllerRevision
+		collisionCount  int32
+		templateType    unitv1alpha1.TemplateType
+		expect          reconcile.Result
+	}{
+		{
+			"equal",
+			yad,
+			&unitv1alpha1.YurtAppDaemonStatus{
+				CurrentRevision:    controllerName,
+				CollisionCount:     &int1,
+				TemplateType:       "StatefulSet",
+				ObservedGeneration: 1,
+				NodePools: []string{
+					"192.168.1.1",
+				},
+				Conditions: []unitv1alpha1.YurtAppDaemonCondition{
+					{
+						Type: unitv1alpha1.WorkLoadProvisioned,
+					},
+				},
+			},
+			&unitv1alpha1.YurtAppDaemonStatus{
+				CurrentRevision:    controllerName,
+				CollisionCount:     &int1,
+				TemplateType:       "StatefulSet",
+				ObservedGeneration: 1,
+				NodePools: []string{
+					"192.168.1.1",
+				},
+				Conditions: []unitv1alpha1.YurtAppDaemonCondition{
+					{
+						Type: unitv1alpha1.WorkLoadProvisioned,
+					},
+				},
+			},
+			&appsv1.ControllerRevision{},
+			int1,
+			"StatefulSet",
+			reconcile.Result{},
+		},
+	}
+
+	for _, tt := range tests {
+		st := tt
+		tf := func(t *testing.T) {
+			t.Parallel()
+			t.Logf("\tTestCase: %s", st.name)
+			{
+				rc := &ReconcileYurtAppDaemon{}
+				get, _ := rc.updateStatus(
+					st.instance, st.newStatus, st.oldStatus, st.currentRevision, st.collisionCount, st.templateType)
+				if !reflect.DeepEqual(get, st.expect) {
+					t.Fatalf("\t%s\texpect %v, but get %v", failed, st.expect, get)
+				}
+				t.Logf("\t%s\texpect %v, get %v", succeed, st.expect, get)
+
+			}
+		}
+		t.Run(st.name, tf)
+	}
+}
 
 func TestUpdateYurtAppDaemon(t *testing.T) {
 	var int1 int32 = 11
@@ -264,6 +321,51 @@ func TestCalculateStatus(t *testing.T) {
 	}
 }
 
+func TestManageWorkloads(t *testing.T) {
+	var yad *unitv1alpha1.YurtAppDaemon
+	yad = &unitv1alpha1.YurtAppDaemon{}
+	yad.Generation = 1
+
+	tests := []struct {
+		name                      string
+		instance                  *unitv1alpha1.YurtAppDaemon
+		currentNodepoolToWorkload map[string]*workloadcontroller.Workload
+		allNameToNodePools        map[string]unitv1alpha1.NodePool
+		expectedRevision          string
+		templateType              unitv1alpha1.TemplateType
+		expect                    bool
+	}{
+		{
+			"normal",
+			yad,
+			map[string]*workloadcontroller.Workload{},
+			map[string]unitv1alpha1.NodePool{},
+			"a",
+			"StatefulSet",
+			false,
+		},
+	}
+
+	for _, tt := range tests {
+		st := tt
+		tf := func(t *testing.T) {
+			t.Parallel()
+			t.Logf("\tTestCase: %s", st.name)
+			{
+				rc := &ReconcileYurtAppDaemon{}
+				rc.manageWorkloads(st.instance, st.currentNodepoolToWorkload, st.allNameToNodePools, st.expectedRevision, st.templateType)
+				get := st.expect
+				if !reflect.DeepEqual(get, st.expect) {
+					t.Fatalf("\t%s\texpect %v, but get %v", failed, st.expect, get)
+				}
+				t.Logf("\t%s\texpect %v, get %v", succeed, st.expect, get)
+
+			}
+		}
+		t.Run(st.name, tf)
+	}
+}
+
 func TestManageWorkloadsProvision(t *testing.T) {
 	var yad *unitv1alpha1.YurtAppDaemon
 	yad = &unitv1alpha1.YurtAppDaemon{}
@@ -355,40 +457,133 @@ func TestClassifyWorkloads(t *testing.T) {
 	}
 }
 
-//func TestGetNodePoolToWorkLoad(t *testing.T) {
-//	var yad *unitv1alpha1.YurtAppDaemon
-//	yad = &unitv1alpha1.YurtAppDaemon{}
-//	yad.Generation = 1
-//
-//	tests := []struct {
-//		name     string
-//		instance *unitv1alpha1.YurtAppDaemon
-//		c        workloadcontroller.DeploymentControllor
-//		expect   map[string]*workloadcontroller.Workload
-//	}{
-//		{
-//			name:     "normal",
-//			instance: yad,
-//			c:        workloadcontroller.DeploymentControllor{},
-//			expect:   map[string]*workloadcontroller.Workload{},
-//		},
-//	}
-//
-//	for _, tt := range tests {
-//		st := tt
-//		tf := func(t *testing.T) {
-//			t.Parallel()
-//			t.Logf("\tTestCase: %s", st.name)
-//			{
-//				rc := &ReconcileYurtAppDaemon{}
-//				get, _ := rc.getNodePoolToWorkLoad(st.instance, &st.c)
-//				if !reflect.DeepEqual(get, st.expect) {
-//					t.Fatalf("\t%s\texpect %v, but get %v", failed, st.expect, get)
-//				}
-//				t.Logf("\t%s\texpect %v, get %v", succeed, st.expect, get)
-//
-//			}
-//		}
-//		t.Run(st.name, tf)
-//	}
-//}
+func TestGetTemplateControls(t *testing.T) {
+	rc := &ReconcileYurtAppDaemon{}
+
+	tests := []struct {
+		name     string
+		instance *unitv1alpha1.YurtAppDaemon
+		expect   unitv1alpha1.TemplateType
+	}{
+		{
+			name: "default",
+			instance: &unitv1alpha1.YurtAppDaemon{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: "kube-system",
+					Name:      "yad",
+				},
+				Spec: unitv1alpha1.YurtAppDaemonSpec{
+					Selector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"a": "a",
+						},
+					},
+				},
+			},
+			expect: "",
+		},
+		{
+			name: "deployment",
+			instance: &unitv1alpha1.YurtAppDaemon{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: "kube-system",
+					Name:      "yad",
+				},
+				Spec: unitv1alpha1.YurtAppDaemonSpec{
+					WorkloadTemplate: unitv1alpha1.WorkloadTemplate{
+						DeploymentTemplate: &unitv1alpha1.DeploymentTemplateSpec{
+							ObjectMeta: metav1.ObjectMeta{
+								Labels: map[string]string{
+									"a": "a",
+								},
+							},
+							Spec: appsv1.DeploymentSpec{
+								Template: v1.PodTemplateSpec{
+									Spec: v1.PodSpec{
+										Volumes: []v1.Volume{},
+										Containers: []v1.Container{
+											{
+												VolumeMounts: []v1.VolumeMount{},
+											},
+										},
+									},
+								},
+								Selector: &metav1.LabelSelector{
+									MatchLabels: map[string]string{
+										unitv1alpha1.PoolNameLabelKey: "a",
+									},
+								},
+							},
+						},
+					},
+					Selector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"a": "a",
+						},
+					},
+				},
+			},
+			expect: unitv1alpha1.DeploymentTemplateType,
+		},
+		{
+			name: "stateful",
+			instance: &unitv1alpha1.YurtAppDaemon{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: "kube-system",
+					Name:      "yad",
+				},
+				Spec: unitv1alpha1.YurtAppDaemonSpec{
+					WorkloadTemplate: unitv1alpha1.WorkloadTemplate{
+						StatefulSetTemplate: &unitv1alpha1.StatefulSetTemplateSpec{
+							ObjectMeta: metav1.ObjectMeta{
+								Labels: map[string]string{
+									"a": "a",
+								},
+							},
+							Spec: appsv1.StatefulSetSpec{
+								Template: v1.PodTemplateSpec{
+									Spec: v1.PodSpec{
+										Volumes: []v1.Volume{},
+										Containers: []v1.Container{
+											{
+												VolumeMounts: []v1.VolumeMount{},
+											},
+										},
+									},
+								},
+								Selector: &metav1.LabelSelector{
+									MatchLabels: map[string]string{
+										unitv1alpha1.PoolNameLabelKey: "a",
+									},
+								},
+							},
+						},
+					},
+					Selector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"a": "a",
+						},
+					},
+				},
+			},
+			expect: unitv1alpha1.StatefulSetTemplateType,
+		},
+	}
+
+	for _, tt := range tests {
+		st := tt
+		tf := func(t *testing.T) {
+			t.Parallel()
+			t.Logf("\tTestCase: %s", st.name)
+			{
+				_, get, _ := rc.getTemplateControls(st.instance)
+				if !reflect.DeepEqual(get, st.expect) {
+					t.Fatalf("\t%s\texpect %v, but get %v", failed, st.expect, get)
+				}
+				t.Logf("\t%s\texpect %v, get %v", succeed, st.expect, get)
+
+			}
+		}
+		t.Run(st.name, tf)
+	}
+}
