f03d17d3b4abd8e8c9afef3b6aafb0fcd063f663
hengxuan.sh <hengxuan.sh@alibaba-inc.com>
2022-10-04 23:32:36 +0800

    update the unit test of uniteddeployment

--- pkg/yurtappmanager/controller/uniteddeployment/revision_test.go
+++ pkg/yurtappmanager/controller/uniteddeployment/revision_test.go
@@ -19,36 +19,411 @@ limitations under the License.
 package uniteddeployment
 
 import (
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	apps "k8s.io/api/apps/v1"
+	"testing"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	fakeclint "sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	appsv1alpha1 "github.com/openyurtio/yurt-app-manager/pkg/yurtappmanager/apis/apps/v1alpha1"
 )
 
-var _ = Describe("Revision", func() {
+func TestReconcileUnitedDeployment_ControlledHistories(t *testing.T) {
 
-	It("revisions length is 0, expect get 1", func() {
-		revisions := []*apps.ControllerRevision{}
-		revisionNum := nextRevision(revisions)
-		Expect(revisionNum).To(Equal(int64(1)))
+	instance := struct {
+		ud  *appsv1alpha1.UnitedDeployment
+		ctr []*appsv1.ControllerRevision
+	}{
+		ud: &appsv1alpha1.UnitedDeployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "foo",
+				Namespace: "default",
+			},
+			Spec: appsv1alpha1.UnitedDeploymentSpec{
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"name": "foo",
+					},
+				},
+				WorkloadTemplate: appsv1alpha1.WorkloadTemplate{
+					DeploymentTemplate: &appsv1alpha1.DeploymentTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Labels: map[string]string{
+								"name": "foo",
+							},
+						},
+						Spec: appsv1.DeploymentSpec{
+							Template: corev1.PodTemplateSpec{
+								ObjectMeta: metav1.ObjectMeta{
+									Labels: map[string]string{
+										"name": "foo",
+									},
+								},
+								Spec: corev1.PodSpec{
+									Containers: []corev1.Container{
+										{
+											Name:  "container-a",
+											Image: "nginx:1.0",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				Topology: appsv1alpha1.Topology{
+					Pools: []appsv1alpha1.Pool{
+						{
+							Name:     "foo-0",
+							Replicas: &one,
+							NodeSelectorTerm: corev1.NodeSelectorTerm{
+								MatchExpressions: []corev1.NodeSelectorRequirement{
+									{
+										Key:      "app.openyurt.io/nodepool",
+										Operator: corev1.NodeSelectorOpIn,
+										Values: []string{
+											"foo-0",
+										},
+									},
+								},
+							},
+						},
+						{
+							Name:     "foo-1",
+							Replicas: &two,
+							NodeSelectorTerm: corev1.NodeSelectorTerm{
+								MatchExpressions: []corev1.NodeSelectorRequirement{
+									{
+										Key:      "app.openyurt.io/nodepool",
+										Operator: corev1.NodeSelectorOpIn,
+										Values: []string{
+											"foo-1",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				RevisionHistoryLimit: &two,
+			},
+		},
+	}
 
-	})
+	scheme := runtime.NewScheme()
+	if err := appsv1alpha1.AddToScheme(scheme); err != nil {
+		t.Logf("failed to add yurt custom resource")
+		return
+	}
+	if err := clientgoscheme.AddToScheme(scheme); err != nil {
+		t.Logf("failed to add kubernetes clint-go custom resource")
+		return
+	}
+	if err := appsv1.AddToScheme(scheme); err != nil {
+		t.Logf("failed to add appsv1 custom resource")
+		return
+	}
 
-	It("last revision number is 1, expect get 2", func() {
-		revisions := []*apps.ControllerRevision{{Revision: 1}}
-		revisionNum := nextRevision(revisions)
-		Expect(revisionNum).To(Equal(int64(2)))
-	})
+	fc := fakeclint.NewClientBuilder().WithScheme(scheme).WithRuntimeObjects(instance.ud).Build()
+	rud := ReconcileUnitedDeployment{
+		Client: fc,
+		scheme: scheme,
+	}
+	histories, err := rud.controlledHistories(instance.ud)
+	if err != nil || len(histories) != 0 {
+		t.Logf("failed to get controlled histories")
+	}
 
-	It("last revision number is 2, expect get 3", func() {
-		revisions := []*apps.ControllerRevision{{Revision: 2}}
-		revisionNum := nextRevision(revisions)
-		Expect(revisionNum).To(Equal(int64(3)))
-	})
+}
 
-	It("last revision number is 10, expect get 11", func() {
-		revisions := []*apps.ControllerRevision{{Revision: 10}}
-		revisionNum := nextRevision(revisions)
-		Expect(revisionNum).To(Equal(int64(11)))
-	})
+func TestReconcileUnitedDeployment_CreateControllerRevision(t *testing.T) {
+	instance := struct {
+		ud  *appsv1alpha1.UnitedDeployment
+		ctr *appsv1.ControllerRevision
+	}{
+		ud: &appsv1alpha1.UnitedDeployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "foo",
+				Namespace: "default",
+			},
+			Spec: appsv1alpha1.UnitedDeploymentSpec{
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"name": "foo",
+					},
+				},
+				WorkloadTemplate: appsv1alpha1.WorkloadTemplate{
+					DeploymentTemplate: &appsv1alpha1.DeploymentTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Labels: map[string]string{
+								"name": "foo",
+							},
+						},
+						Spec: appsv1.DeploymentSpec{
+							Template: corev1.PodTemplateSpec{
+								ObjectMeta: metav1.ObjectMeta{
+									Labels: map[string]string{
+										"name": "foo",
+									},
+								},
+								Spec: corev1.PodSpec{
+									Containers: []corev1.Container{
+										{
+											Name:  "container-a",
+											Image: "nginx:1.0",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				Topology: appsv1alpha1.Topology{
+					Pools: []appsv1alpha1.Pool{
+						{
+							Name: "pool-a",
+							NodeSelectorTerm: corev1.NodeSelectorTerm{
+								MatchExpressions: []corev1.NodeSelectorRequirement{
+									{
+										Key:      "node-name",
+										Operator: corev1.NodeSelectorOpIn,
+										Values:   []string{"nodeA"},
+									},
+								},
+							},
+						},
+					},
+				},
+				RevisionHistoryLimit: &two,
+			},
+		},
+		ctr: &appsv1.ControllerRevision{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "foo-0",
+				Namespace: "foo-ns",
+			},
+			Revision: 1,
+		},
+	}
+	scheme := runtime.NewScheme()
+	if err := appsv1alpha1.AddToScheme(scheme); err != nil {
+		t.Logf("failed to add yurt custom resource")
+		return
+	}
+	if err := clientgoscheme.AddToScheme(scheme); err != nil {
+		t.Logf("failed to add kubernetes clint-go custom resource")
+		return
+	}
+	if err := appsv1.AddToScheme(scheme); err != nil {
+		t.Logf("failed to add appsv1 custom resource")
+		return
+	}
 
-})
+	fc := fakeclint.NewClientBuilder().WithScheme(scheme).WithRuntimeObjects(instance.ud).Build()
+	rud := ReconcileUnitedDeployment{
+		Client: fc,
+		scheme: scheme,
+	}
+	o, err := rud.createControllerRevision(instance.ud, instance.ctr, &two)
+	if err != nil && o.Revision != 1 {
+		t.Logf("failed to create controller revision")
+	}
+}
+
+func TestReconcileUnitedDeployment_newRevision(t *testing.T) {
+	instance := &appsv1alpha1.UnitedDeployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "foo-ns",
+		},
+		Spec: appsv1alpha1.UnitedDeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "foo",
+				},
+			},
+			WorkloadTemplate: appsv1alpha1.WorkloadTemplate{
+				StatefulSetTemplate: &appsv1alpha1.StatefulSetTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "foo",
+						Labels: map[string]string{
+							"name": "foo",
+						},
+					},
+					Spec: appsv1.StatefulSetSpec{
+						Replicas: &two,
+						Template: corev1.PodTemplateSpec{
+							ObjectMeta: metav1.ObjectMeta{
+								Labels: map[string]string{
+									"name": "foo",
+								},
+							},
+							Spec: corev1.PodSpec{
+								Containers: []corev1.Container{
+									{
+										Name:  "nginx",
+										Image: "nginx:1.19",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			Topology: appsv1alpha1.Topology{
+				Pools: []appsv1alpha1.Pool{
+					{
+						Name:     "foo-0",
+						Replicas: &one,
+						NodeSelectorTerm: corev1.NodeSelectorTerm{
+							MatchExpressions: []corev1.NodeSelectorRequirement{
+								{
+									Key:      "app.openyurt.io/nodepool",
+									Operator: corev1.NodeSelectorOpIn,
+									Values: []string{
+										"foo-0",
+									},
+								},
+							},
+						},
+					},
+					{
+						Name:     "foo-1",
+						Replicas: &two,
+						NodeSelectorTerm: corev1.NodeSelectorTerm{
+							MatchExpressions: []corev1.NodeSelectorRequirement{
+								{
+									Key:      "app.openyurt.io/nodepool",
+									Operator: corev1.NodeSelectorOpIn,
+									Values: []string{
+										"foo-1",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	scheme := runtime.NewScheme()
+	if err := appsv1alpha1.AddToScheme(scheme); err != nil {
+		t.Logf("failed to add yurt custom resource")
+		return
+	}
+	if err := clientgoscheme.AddToScheme(scheme); err != nil {
+		t.Logf("failed to add kubernetes clint-go custom resource")
+		return
+	}
+	if err := appsv1.AddToScheme(scheme); err != nil {
+		t.Logf("failed to add appsv1 custom resource")
+		return
+	}
+
+	fc := fakeclint.NewClientBuilder().WithScheme(scheme).WithRuntimeObjects().Build()
+	rud := ReconcileUnitedDeployment{
+		Client: fc,
+		scheme: scheme,
+	}
+	cr, err := rud.newRevision(instance, 2, &two)
+	if err != nil && cr.Namespace != instance.Namespace {
+		t.Logf("failed to new revision for yurtappset")
+	}
+}
+
+func TestReconcileUnitedDeployment_ConstructYurtAppSetRevisions(t *testing.T) {
+	instance := struct {
+		ud  *appsv1alpha1.UnitedDeployment
+		ctr *appsv1.ControllerRevision
+	}{
+		ud: &appsv1alpha1.UnitedDeployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "foo",
+				Namespace: "foo-ns",
+			},
+			Spec: appsv1alpha1.UnitedDeploymentSpec{
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"name": "foo",
+					},
+				},
+				WorkloadTemplate: appsv1alpha1.WorkloadTemplate{
+					DeploymentTemplate: &appsv1alpha1.DeploymentTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Labels: map[string]string{
+								"name": "foo",
+							},
+						},
+						Spec: appsv1.DeploymentSpec{
+							Template: corev1.PodTemplateSpec{
+								ObjectMeta: metav1.ObjectMeta{
+									Labels: map[string]string{
+										"name": "foo",
+									},
+								},
+								Spec: corev1.PodSpec{
+									Containers: []corev1.Container{
+										{
+											Name:  "container-a",
+											Image: "nginx:1.0",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				Topology: appsv1alpha1.Topology{
+					Pools: []appsv1alpha1.Pool{
+						{
+							Name: "pool-a",
+							NodeSelectorTerm: corev1.NodeSelectorTerm{
+								MatchExpressions: []corev1.NodeSelectorRequirement{
+									{
+										Key:      "node-name",
+										Operator: corev1.NodeSelectorOpIn,
+										Values:   []string{"nodeA"},
+									},
+								},
+							},
+						},
+					},
+				},
+				RevisionHistoryLimit: &two,
+			},
+		},
+		ctr: &appsv1.ControllerRevision{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "foo-0",
+				Namespace: "foo-ns",
+			},
+			Revision: 1,
+		},
+	}
+	scheme := runtime.NewScheme()
+	if err := appsv1alpha1.AddToScheme(scheme); err != nil {
+		t.Logf("failed to add yurt custom resource")
+		return
+	}
+	if err := clientgoscheme.AddToScheme(scheme); err != nil {
+		t.Logf("failed to add kubernetes clint-go custom resource")
+		return
+	}
+	if err := appsv1.AddToScheme(scheme); err != nil {
+		t.Logf("failed to add appsv1 custom resource")
+		return
+	}
+
+	fc := fakeclint.NewClientBuilder().WithScheme(scheme).WithRuntimeObjects(instance.ud).Build()
+	rud := ReconcileUnitedDeployment{
+		Client: fc,
+		scheme: scheme,
+	}
+	_, _, _, err := rud.constructUnitedDeploymentRevisions(instance.ud)
+	if err != nil {
+		t.Logf("failed to construct uniteddeployment revisions")
+	}
+}
